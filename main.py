0907e91994910b8b8b3bc3c2d95c7d1c5a3b0a09
Dark Reaper <darkReaperDev@gmail.com>
2020-09-26 23:10:31 +0200

    edited main.py

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ GITHUB_REPO_NAME = "Code2UML"
 
 def main():
     argv = sys.argv
-    for var in argv:
-        print(var)
+    try:
+        file = open("artifactsTest.txt", "a")
+    
+        for var in argv:
+            file.write(var)
 
+        file.close()
+    except:
+        print("writing failed")
 
 main()
