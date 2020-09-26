45f97481fa46a1ccbfaaa17602542291cb0d049b
Dark Reaper <darkReaperDev@gmail.com>
2020-09-26 22:56:19 +0200

    edited main.py

--- main.py
+++ main.py
@@ -8,12 +8,9 @@ GITHUB_REPO_OWNER_NAME = "DarkReaperDev"
 GITHUB_REPO_NAME = "Code2UML"
 
 def main():
-    http = urllib3.PoolManager()
-    result = http.request("GET", "https://api.github.com/repos/" + GITHUB_REPO_OWNER_NAME + "/" + GITHUB_REPO_NAME + "/events")
-    print(result.data)
-    jsonResult = json.loads(result.data)
-    print(jsonResult)
-
+    argv = sys.argv
+    for var in argv:
+        print(var)
 
 
 main()
