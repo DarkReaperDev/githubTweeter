4ac01e4feb867574325133e1ae6b4e7465a7229d
Dark Reaper <darkReaperDev@gmail.com>
2020-09-27 14:58:07 +0200

    edited main.py file

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import json
 import sys
 
 TWITTER_API_KEY = ""
-GITHUB_API_KEY = ""
-
-GITHUB_REPO_OWNER_NAME = "DarkReaperDev"
-GITHUB_REPO_NAME = "Code2UML"
 
 def main():
     message = sys.argv[1]
