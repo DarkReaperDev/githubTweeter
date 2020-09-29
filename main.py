f8e4f11e1076eff44273c8f9538409442d695b2a
DarkReaperDev <51919602+DarkReaperDev@users.noreply.github.com>
2020-09-29 13:33:39 +0200

    edited main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import sys
 
 def main():
     message = sys.argv[1][1:-1]
+    TWITTER_API_KEY = sys.argv[2]
+    TWITTER_API_KEY_SECRET = sys.argv[3]
+    TWITTER_ACCESS_TOKEN = sys.argv[4]
+    TWITTER_ACCESS_TOKEN_SECRET = sys.argv[5]
     
     file = open("artifactsTest.txt", "a")
     
@@ -12,5 +16,9 @@ def main():
     file.write("\n")    
 
     file.close()
+
+
+    if "//tweet" in message.lower():
+        
 
 main()
