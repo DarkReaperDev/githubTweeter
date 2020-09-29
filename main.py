aebe2f18d8abe0456b96ecc7ec584047c3a3bfb8
DarkReaperDev <51919602+DarkReaperDev@users.noreply.github.com>
2020-09-29 17:00:29 +0200

    testing githubTweeter //tweet

--- main.py
+++ main.py
@@ -20,19 +20,13 @@ def main():
     if "//tweet" in message.lower():
 
         auth = tweepy.OAuthHandler(
-
             twitter_auth_keys['consumer_key'],
-
             twitter_auth_keys['consumer_secret']
-
         )
 
         auth.set_access_token(
-
             twitter_auth_keys['access_token'],
-
             twitter_auth_keys['access_token_secret']
-
         )
 
         api = tweepy.API(auth)
